171946a3eeac7d24e8e84d8235692d47c64a9258
nanagami1369 <nanagami1369@gmail.com>
2020-03-15 23:06:15 +0900

    共通で使うUIをまとめておくライブラリーを作成

---
+++ CommonUILibrary/CommonUILibraryModule.cs
@@ -0,0 +1,19 @@
+﻿using CommonUILibrary.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace CommonUILibrary
+{
+    public class CommonUILibraryModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
