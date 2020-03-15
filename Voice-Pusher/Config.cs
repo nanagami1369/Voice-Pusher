b195c87147bae603953963a93f8e67c1a8ec3c3c
nanagami1369 <nanagami1369@gmail.com>
2020-03-15 15:19:39 +0900

    設定ファイルを作成

---
+++ Voice-Pusher/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Voice_Pusher
+{
+    public static class Config
+    {
+        public static string Title = "Voice-Pusher";
+    }
+}
