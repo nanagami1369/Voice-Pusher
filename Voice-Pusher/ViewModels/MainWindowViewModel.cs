61eefb00bc48b0a31111ba29c0d59b8a1f0d75cb
nanagami1369 <nanagami1369@gmail.com>
2020-03-15 15:19:55 +0900

    Prismに対応させた

---
+++ Voice-Pusher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+
+namespace Voice_Pusher.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = Config.Title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
