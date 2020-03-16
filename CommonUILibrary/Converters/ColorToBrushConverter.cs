14f13ae925956d34157b8211bb8db8c32f8aab78
nanagami1369 <nanagami1369@gmail.com>
2020-03-16 15:54:20 +0900

    System.Dorwing.Colorクラスをブラシクラスに変換するコンバーターを作成

---
+++ CommonUILibrary/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CommonUILibrary.Converters
+{
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is System.Drawing.Color color)) return Binding.DoNothing;
+            var mediaColor = System.Windows.Media.Color.FromRgb(color.R, color.G, color.B);
+            return new SolidColorBrush(mediaColor);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
