3b6dd12cd6f0b8ccf253561ad76213ba6e0ca3db
nanagami1369 <nanagami1369@gmail.com>
2020-03-23 23:40:04 +0900

    どの合成音声ライブラリーにも属さないキャラクタクラスを作成

---
+++ CommonLibrary/Modules/CharacterLibraryModule/PartialCharacter.cs
@@ -0,0 +1,8 @@
+﻿namespace CommonLibrary.Modules.CharacterLibraryModule
+{
+    public class PartialCharacter : ICharacter
+    {
+        public string Name { get; set; }
+        public string Reading { get; set; }
+    }
+}
