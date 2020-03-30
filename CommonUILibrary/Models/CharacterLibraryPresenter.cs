1a631791a96c35fdda6f461c4f19443614492f2c
nanagami1369 <nanagami1369@gmail.com>
2020-03-30 22:12:22 +0900

    UIを処理するクラスを作成

---
+++ CommonUILibrary/Models/CharacterLibraryPresenter.cs
@@ -0,0 +1,122 @@
+﻿using CommonLibrary;
+using CommonLibrary.Modules.CharacterLibraryModule;
+using CommonLibrary.Modules.StatusModule;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CommonUILibrary.Models
+{
+    public class CharacterLibraryPresenter : BindableBase, ICharacterLibraryPresenter
+    {
+        private readonly IDialog _dialog;
+        private readonly IStatusSender _statusSender;
+        private readonly ICharacterLibraryGateway _gateway;
+
+        private ObservableCollection<ICharacter> _selectedLibrary;
+        public ObservableCollection<ICharacter> SelectedLibrary
+        {
+            get => _selectedLibrary;
+            set => SetProperty(ref _selectedLibrary, value);
+        }
+
+        private ICharacter _selectedCharacter;
+
+        public ICharacter SelectedCharacter
+        {
+            get => _selectedCharacter;
+            set => SetProperty(ref _selectedCharacter, value);
+        }
+
+        public void SearchCharacter(string query)
+        {
+            var originalCharacterLibrary = _gateway.Read();
+            if (string.IsNullOrEmpty(query))
+            {
+                SelectedLibrary = new ObservableCollection<ICharacter>(originalCharacterLibrary);
+                return;
+            }
+            var searchedLibrary = new ObservableCollection<ICharacter>();
+            searchedLibrary.AddRange(originalCharacterLibrary.AsParallel().Where(item => item.Name == query));
+            searchedLibrary.AddRange(originalCharacterLibrary.Where(item => item.Reading.StartsWith(query)));
+            SelectedLibrary = searchedLibrary;
+            // ListBoxのセレクターがリセットされるので再設定
+            Index = 0;
+        }
+
+        public void SelectCharacter()
+        {
+            _dialog.ShowMessage("モック", $"まだ作ってないよ\n選択中：{SelectedCharacter.Name}");
+        }
+
+        public CharacterLibraryPresenter(IStatusSender statusSender, IDialog dialog, ICharacterLibraryGateway gateway)
+        {
+            _dialog = dialog;
+            _statusSender = statusSender;
+            _gateway = gateway;
+            var originalCharacterLibrary = _gateway.Read();
+            SelectedLibrary = new ObservableCollection<ICharacter>(originalCharacterLibrary);
+        }
+
+        #region keyboardActions
+
+        private bool _isFocus;
+
+        public bool IsFocus
+        {
+            get => _isFocus;
+            set => SetProperty(ref _isFocus, value);
+        }
+
+        private int _index;
+
+        public int Index
+        {
+            get => _index;
+            set => SetProperty(ref _index, value);
+        }
+
+
+        public void UpCharacterLibrary()
+        {
+            if (SelectedLibrary == null) return;
+            if (Index <= 0)
+            {
+                Index = SelectedLibrary.Count - 1;
+            }
+            else
+            {
+                Index--;
+            }
+        }
+
+        public void DownCharacterLibrary()
+        {
+            if (SelectedLibrary == null) return;
+            if (Index >= SelectedLibrary.Count - 1)
+            {
+                Index = 0;
+            }
+            else
+            {
+                Index++;
+            }
+        }
+
+        public void ResetSelector()
+        {
+            if (SelectedLibrary != null && Index == -1)
+            {
+                Index = 0;
+            }
+        }
+
+        public void SetFocus()
+        {
+            IsFocus = true;
+            IsFocus = false;
+        }
+    }
+
+    #endregion
+}
--- CommonUILibrary/ViewModels/CharacterLibraryViewModel.cs
+++ CommonUILibrary/ViewModels/CharacterLibraryViewModel.cs
@@ -1,12 +1,35 @@
-﻿using Prism.Mvvm;
+﻿using CommonLibrary.Modules.CharacterLibraryModule;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace CommonUILibrary.ViewModels
 {
     public class CharacterLibraryViewModel : BindableBase
     {
-        public CharacterLibraryViewModel()
+        public ICharacterLibraryPresenter CharacterLibrary { get; }
+
+        public DelegateCommand SelectCharacterCommand { get; }
+
+        public DelegateCommand<string> SearchCharacterCommand { get; }
+
+        #region keyboardActions
+
+        public DelegateCommand UpCharacterLibraryCommand { get; }
+        public DelegateCommand DownCharacterLibraryCommand { get; }
+        public DelegateCommand ResetSelectorCommand { get; }
+        public DelegateCommand SetFocusCommand { get; }
+
+        #endregion
+
+        public CharacterLibraryViewModel(ICharacterLibraryPresenter characterLibrary)
         {
-            
+            CharacterLibrary = characterLibrary;
+            SelectCharacterCommand = new DelegateCommand(CharacterLibrary.SelectCharacter);
+            SearchCharacterCommand = new DelegateCommand<string>(CharacterLibrary.SearchCharacter);
+            UpCharacterLibraryCommand = new DelegateCommand(CharacterLibrary.UpCharacterLibrary);
+            DownCharacterLibraryCommand = new DelegateCommand(CharacterLibrary.DownCharacterLibrary);
+            ResetSelectorCommand = new DelegateCommand(CharacterLibrary.ResetSelector);
+            SetFocusCommand = new DelegateCommand(CharacterLibrary.SetFocus);
         }
     }
 }
