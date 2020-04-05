6eacee55c272a1153275b50e539b268cecb50029
nanagami1369 <nanagami1369@gmail.com>
2020-04-05 21:37:31 +0900

    声優情報を保持するクラスを作成

---
+++ CommonLibrary/Modules/CharacterLibraryModule/VoiceActor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CommonLibrary.Modules.CharacterLibraryModule
+{
+    public class VoiceActor : IEquatable<VoiceActor>
+    {
+        public string Name { get; set; }
+        public string Office { get; set; }
+        public float Version { get; set; }
+        public string DisplayName => $"{Name}:{Office}:{Version}";
+
+        public bool Equals(VoiceActor actor)
+        {
+            return (Name == actor?.Name &&
+                    Office == actor?.Office &&
+                    Version == actor?.Version);
+        }
+
+        public VoiceActor(string name, string office, float version)
+        {
+            Name = name;
+            Office = office;
+            Version = version;
+        }
+
+    }
+}
