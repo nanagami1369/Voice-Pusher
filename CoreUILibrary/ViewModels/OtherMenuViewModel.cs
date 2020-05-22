70d3da369fc5827a97e262ecca3e951dc9214660
nanagami1369 <nanagami1369@gmail.com>
2020-05-23 07:36:51 +0900

    {アプリ名}についてが表示されないバグを修正

--- CoreUILibrary/ViewModels/OtherMenuViewModel.cs
+++ CoreUILibrary/ViewModels/OtherMenuViewModel.cs
@@ -1,13 +1,11 @@
-using Prism.Commands;
+using CommonLibrary;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace CoreUILibrary.ViewModels
 {
     public class OtherMenuViewModel : BindableBase
     {
+        public string Title => Config.Title;
         public OtherMenuViewModel()
         {
 
