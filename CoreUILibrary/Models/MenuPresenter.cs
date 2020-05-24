182f82b6bff4fe152a849c34cbcacd7ff96df474
nanagami1369 <nanagami1369@gmail.com>
2020-05-25 00:23:02 +0900

    現在のメニューの選択状態を登録出来るようにした。

--- CoreUILibrary/Models/MenuPresenter.cs
+++ CoreUILibrary/Models/MenuPresenter.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using CommonLibrary;
 using CommonLibrary.Modules.MenuModule;
+using CommonUILibrary.Models;
 using Prism.Mvvm;
 
 namespace CoreUILibrary.Models
@@ -8,6 +9,7 @@ namespace CoreUILibrary.Models
     public class MenuPresenter : BindableBase, IMenuPresenter
     {
         private readonly IViewSelectable _viewSelectable;
+        private readonly MenuContainer _container;
         public MenuItem[] MenuList { get; }
 
         private MenuItem _selectedMenu;
@@ -20,13 +22,16 @@ namespace CoreUILibrary.Models
         public void ChangeView()
         {
             _viewSelectable.ChangeContentView(SelectedMenu.ViewName);
+            _container.Register(SelectedMenu);
         }
 
-        public MenuPresenter(IViewSelectable viewSelectable)
+        public MenuPresenter(IViewSelectable viewSelectable, MenuContainer container)
         {
             _viewSelectable = viewSelectable;
             MenuList = Config.MenuItem;
             SelectedMenu = MenuList.FirstOrDefault();
+            _container = container;
+            _container.Register(SelectedMenu);
         }
 
         #region keyboardActions
