7bf9f61be3aac4db5841985c7f318b822cdbff3e
nanagami1369 <nanagami1369@gmail.com>
2020-05-24 10:46:22 +0900

    設定を保持するクラスの動作確認用に、取得した設定を表示するボタンを作成

--- Test/UITest/ViewModels/MainWindowViewModel.cs
+++ Test/UITest/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,11 @@
-﻿using CommonLibrary.Modules.StatusModule;
+using CommonLibrary;
+using CommonLibrary.Modules.SettingModule;
+using CommonLibrary.Modules.StatusModule;
 using CommonUILibrary.Commands;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
+using CommonUILibrary.Setting;
 
 namespace UITest.ViewModels
 {
@@ -10,6 +13,8 @@ namespace UITest.ViewModels
     {
         private readonly IStatusSender _statusSender;
         private readonly IRegionManager _regionManager;
+        private readonly ISettingContainer _settingManager;
+        private readonly IDialog _dialog;
         private string _title = "UTest";
 
         public string Title
@@ -18,6 +23,26 @@ namespace UITest.ViewModels
             set => SetProperty(ref _title, value);
         }
 
+        private ISetting Setting => _settingManager.Read();
+
+        private void ShowSetting()
+        {
+            var setting = Setting;
+            var settingString = @$"
+  Common:
+    OutPutDirectoryPath: {setting.Common.OutPutDirectoryPath},
+    OutPutTextEncode: {setting.Common.OutPutTextEncode.CodePage},
+    IsLogWrite: {setting.Common.IsLogWrite},
+    NameScript: {setting.Common.NameScript}
+  ,
+  Script:
+    CsvEncode: {setting.Script.CsvEncode.CodePage},
+    OutputMode: {setting.Script.OutputMode}
+";
+            _dialog.ShowMessage("設定内容", settingString);
+        }
+
+        public DelegateCommand ShowSettingCommand { get; }
         public DelegateCommand ChangeStatusBarCommand { get; }
         private int Counter { get; set; }
 
@@ -64,14 +89,20 @@ namespace UITest.ViewModels
             set => SetProperty(ref _applicationCommands, value);
         }
 
-        public MainWindowViewModel(IStatusSender statusSender, IRegionManager regionManager, IApplicationCommands applicationCommands)
+        public MainWindowViewModel(IStatusSender statusSender,
+            IRegionManager regionManager,
+            ISettingContainer settingManager,
+            IDialog dialog,
+            IApplicationCommands applicationCommands)
         {
             _statusSender = statusSender;
             _regionManager = regionManager;
+            _settingManager = settingManager;
+            _dialog = dialog;
             ApplicationCommands = applicationCommands;
+            ShowSettingCommand = new DelegateCommand(ShowSetting);
             ChangeStatusBarCommand = new DelegateCommand(ChangeStatusBar);
             ChangeCharacterEditorCommand = new DelegateCommand(ChangeCharacterEditor);
-
         }
     }
 }
