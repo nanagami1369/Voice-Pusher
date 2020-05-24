d808462faac18460e780a15a637f33b0a9021682
nanagami1369 <nanagami1369@gmail.com>
2020-05-25 00:17:17 +0900

    メニュークラスにコピーメソッドを作成

--- CommonLibrary/Modules/MenuModule/MenuItem.cs
+++ CommonLibrary/Modules/MenuModule/MenuItem.cs
@@ -2,10 +2,19 @@ namespace CommonLibrary.Modules.MenuModule
 {
     public class MenuItem
     {
-        public string Name { get; }
-        public string ViewName { get; }
-        public string IconName { get; }
+        public string Name { get; private set; }
+        public string ViewName { get; private set; }
+        public string IconName { get; private set; }
 
+        public MenuItem Copy()
+        {
+            return new MenuItem("", "", "")
+            {
+                Name = this.Name,
+                ViewName = this.ViewName,
+                IconName = this.IconName,
+            };
+        }
         public MenuItem(string name, string viewName, string iconName)
         {
             Name = name;
