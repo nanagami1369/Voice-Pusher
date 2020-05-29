9290f8dc7ce691d174b402e1c0929c7d5b74d682
nanagami1369 <nanagami1369@gmail.com>
2020-05-29 14:07:01 +0900

    Speech Application Programming用の合成音声ライブラリーを追加

---
+++ VoiceLibrary/SAP/SAPModule.cs
@@ -0,0 +1,19 @@
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace SAP
+{
+    public class SAPModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
