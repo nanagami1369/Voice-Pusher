a6cfd8894ccf8fe422ef15e9d4319f28ea3c0845
nanagami1369 <nanagami1369@gmail.com>
2020-06-04 18:51:53 +0900

    クラス内部でのみ使うインターフェイスがpublicになってしまっていたのを修正

--- CommonUILibrary/Models/Dialog.cs
+++ CommonUILibrary/Models/Dialog.cs
@@ -56,7 +56,7 @@ namespace CommonUILibrary.Models
         [ComImport]
         [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
         [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        public interface IInitializeWithWindow
+        private interface IInitializeWithWindow
         {
             void Initialize(IntPtr hwnd);
         }
