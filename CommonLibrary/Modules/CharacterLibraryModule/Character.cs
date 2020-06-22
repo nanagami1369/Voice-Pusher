89ebec5a4dc328bebe3cbca599c6241494fc132d
nanagami1369 <nanagami1369@gmail.com>
2020-06-23 02:35:19 +0900

    セリフから字幕に使うテキストだけを抜き出すメソッドを追加

--- CommonLibrary/Modules/CharacterLibraryModule/Character.cs
+++ CommonLibrary/Modules/CharacterLibraryModule/Character.cs
@@ -5,5 +5,7 @@ namespace CommonLibrary.Modules.CharacterLibraryModule
         public string Name { get; set; }
         public string Reading { get; set; }
         public VoiceActor VoiceActor { get; set; }
+
+        public abstract string ScriptToOutputText(string script);
     }
 }
