399f9847295c4670bd87e19f559d945723733d10
nanagami1369 <nanagami1369@gmail.com>
2020-08-25 13:16:08 +0900

    コンテナに登録されたショートカットキーを実際に使えるようにした

--- Test/UITest/ViewModels/MainWindowViewModel.cs
+++ Test/UITest/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using CommonLibrary.Modules.MenuModule;
 using CommonLibrary.Modules.SettingModule;
 using CommonLibrary.Modules.StatusModule;
 using CommonUILibrary.Commands;
+using CommonUILibrary.Models;
 using Prism.Commands;
 using Prism.Mvvm;
 
@@ -73,6 +74,8 @@ namespace UITest.ViewModels
             }
         }
 
+        public IHotKeyContainer HotKeyContainer { get; }
+
         private IApplicationCommands _applicationCommands;
         public IApplicationCommands ApplicationCommands
         {
@@ -85,12 +88,14 @@ namespace UITest.ViewModels
             ISettingContainer settingManager,
             IDialog dialog,
             IMenuContainerReader menuContainer,
+            IHotKeyContainer hotKeyContainer,
             IApplicationCommands applicationCommands)
         {
             _statusSender = statusSender;
             _settingManager = settingManager;
             _dialog = dialog;
             _menuContainer = menuContainer;
+            HotKeyContainer = hotKeyContainer;
             ApplicationCommands = applicationCommands;
             ShowSettingCommand = new DelegateCommand(async () => await ShowSettingAsync());
             ShowMenuCommand = new DelegateCommand(async () => await ShowMenuAsync());
