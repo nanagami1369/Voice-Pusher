62725664073554e79a8ce005deaa81fe4f9b9715
nanagami1369 <nanagami1369@gmail.com>
2020-12-12 00:24:24 +0900

    SAPのVoiceActorを登録

--- VoiceLibrary/SAP/SAPModule.cs
+++ VoiceLibrary/SAP/SAPModule.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using CommonLibrary.Modules.CharacterLibraryModule;
 using Prism.Ioc;
 using Prism.Modularity;
-using Prism.Regions;
+using SAP.Models;
+using Windows.Media.SpeechSynthesis;
 
 namespace SAP
 {
@@ -8,12 +13,20 @@ namespace SAP
     {
         public void OnInitialized(IContainerProvider containerProvider)
         {
-
+            var catalog = containerProvider.Resolve<IVoiceActorCatalog>();
+            catalog.Register(VoiceActorCatalog);
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
 
         }
+
+        public static List<VoiceActor> VoiceActorCatalog => SpeechSynthesizer
+        .AllVoices
+        .Where(voice => string.Compare(voice.Language, "ja-JP") == 0)
+        .Select(voice => new SAPVoiceActor(voice.DisplayName, voice.Language))
+        .Select(actor => (VoiceActor)actor)
+        .ToList();
     }
 }
