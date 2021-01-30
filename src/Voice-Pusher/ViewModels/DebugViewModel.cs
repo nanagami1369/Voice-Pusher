2bbd10c3976c8a34341fac3d74253ba34a0fb511
nanagami1369 <nanagami1369@gmail.com>
2021-01-31 00:29:10 +0900

    ステータスを自由に設定できる機能をデバッグ画面に追加

--- src/Voice-Pusher/ViewModels/DebugViewModel.cs
+++ src/Voice-Pusher/ViewModels/DebugViewModel.cs
@@ -1,8 +1,48 @@
+using System.Linq;
+using CoreLibrary;
+using CoreLibrary.StatusModels;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace Voice_Pusher.ViewModels
 {
     public class DebugViewModel : BindableBase
     {
+        private string _message = "";
+        private StatusLevel _selectedStatusLevel;
+
+        public DebugViewModel(IDataContainer container)
+        {
+            _selectedStatusLevel = StatusLevels.First();
+            Container = container;
+            SendMessageCommand = new DelegateCommand(SendMessage);
+        }
+
+        public IDataContainer Container { get; }
+
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+
+        public DelegateCommand SendMessageCommand { get; }
+
+        public StatusLevel SelectedStatusLevel
+        {
+            get => _selectedStatusLevel;
+            set => SetProperty(ref _selectedStatusLevel, value);
+        }
+
+        public StatusLevel[] StatusLevels { get; } =
+        {
+            StatusLevel.Success, StatusLevel.Log, StatusLevel.Warning, StatusLevel.Error
+        };
+
+        private void SendMessage()
+        {
+            Container.CurrentStatus
+                = new Status(SelectedStatusLevel, Message);
+        }
     }
 }
