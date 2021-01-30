d4b715894a7f6e16797f46da597c461717dc71f4
nanagami1369 <nanagami1369@gmail.com>
2021-01-30 14:10:58 +0900

    ふ

--- src/Voice-Pusher/HamburgerMenuItem.cs
+++ src/Voice-Pusher/HamburgerMenuItem.cs
@@ -2,7 +2,7 @@ namespace Voice_Pusher
 {
     public class HamburgerMenuItem
     {
-        public HamburgerMenuItem(string name, string pagekey,string iconName)
+        public HamburgerMenuItem(string name, string pagekey, string iconName)
         {
             Name = name;
             PageKey = pagekey;
