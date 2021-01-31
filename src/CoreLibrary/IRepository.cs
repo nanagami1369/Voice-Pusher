d01f525a165f0ae13a638262ded0ecd00035e724
nanagami1369 <nanagami1369@gmail.com>
2021-01-31 22:54:40 +0900

    REpositoryのジェネリックを参照型に制限する理由が無かったので削除

--- src/CoreLibrary/IRepository.cs
+++ src/CoreLibrary/IRepository.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace CoreLibrary
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T>
     {
         public Task<T> ReadAsync();
         public Task WriterAsync(T saveObject);
