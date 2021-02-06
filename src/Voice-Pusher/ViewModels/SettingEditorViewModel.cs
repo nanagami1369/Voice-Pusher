f286a2fc8f0ffb4fcecb5d97db0ee77006cb36d2
nanagami1369 <nanagami1369@gmail.com>
2021-02-06 21:56:58 +0900

    出力時の文字コードの設定項目で文字コードの選択肢を表示できるようにした

--- src/Voice-Pusher/ViewModels/SettingEditorViewModel.cs
+++ src/Voice-Pusher/ViewModels/SettingEditorViewModel.cs
@@ -1,8 +1,19 @@
+using System.Text;
 using Prism.Mvvm;
 
 namespace Voice_Pusher.ViewModels
 {
     public class SettingEditorViewModel : BindableBase
     {
+        public SettingEditorViewModel()
+        {
+            // Shift-Jisを使えるようにする
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var shiftJis = Encoding.GetEncoding(932);
+            var utf8 = new UTF8Encoding(false);
+            EncodingList = new[] {shiftJis, utf8};
+        }
+
+        public Encoding[] EncodingList { get; }
     }
 }
